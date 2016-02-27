a51046c21d2a5e7c69a240add4b4a991d98b2335
Sugat Poudel <sp5pe@virginia.edu>
2016-02-27 14:06:51 -0500

    working on validation

--- lytics/js/sign_up.js
+++ lytics/js/sign_up.js
@@ -7,12 +7,15 @@ var city_valid = false;
 var state_valid = false;
 var zip_valid = false;
 
+//var fields = {"first_name", "last_name", }
+
 function check_input(element) {
 	
 	if (element.value != "") {
 		element.style.borderLeft = "6px solid #40A46F";
 
 		if (element.name == "first_name") {
+			document.getElementById("name_error").innerHTML = "";
 			f_name_valid = true;
 
 		} else if (element.name == "last_name") {
@@ -48,14 +51,18 @@ function check_input(element) {
 			if (pwd.value != element.value) {
 				element.style.borderLeft = "6px solid red";
 				pwd_error.innerHTML = "retyped address does not match";
+				password_valid = false;
 			} else {
+				pwd_error.innerHTML = "";
 				password_valid = true;
 			}
 		
 		} else if (element.name == "address") {
+			document.getElementById("adr_city_error").innerHTML = "";
 			address_valid = true;
 
 		} else if (element.name == "city") {
+			document.getElementById("adr_city_error").innerHTML = "";
 			city_valid = true;
  
 		} else if (element.name == "state") {
@@ -65,26 +72,44 @@ function check_input(element) {
 
 			if (!check) {
 				element.style.borderLeft = "6px solid red";
-				error.innerHTML = "invalid email address";
+				error.innerHTML = "invalid state acronym e.g. VA";
+				state_valid = false;
 			} else {
 				element.style.borderLeft = "6px solid #40A46F";
 				error.innerHTML = "";
 				state_valid = true;
 			}
 
-		} else if (element.name == "zip") {
+		} else if (element.name == "zip_code") {
+			var re = /[0-9]{5,10}/;
+			var check = re.test(element.value);
+			var error = document.getElementById("state_zip_error");
 
- 			zip_valid = true;
+			if (!check) {
+				element.style.borderLeft = "6px solid red";
+				error.innerHTML = "invalid zip code";
+				zip_valid = false;
+			} else {
+				element.style.borderLeft = "6px solid #40A46F";
+				error.innerHTML = "";
+				zip_valid = true;
+			}
 
 		}
 
 	} else {
+		var name = element.name;
+		if (name == "first_name"|| name == "last_name")
+			document.getElementById("name_error").innerHTML = "";
+
+
 		element.style.borderLeft = "";
 	}
 }
 
 function validate_submit() {
 	var success = true;
+	var error = null;
 
 	if (!f_name_valid || !l_name_valid) {
 		success = false;
@@ -96,7 +121,7 @@ function validate_submit() {
 		if (!l_name_valid) {
 			document.getElementsByName("last_name")[0].style.borderLeft = "6px solid red";
 		}
-	}
+	} 
 
 	if (!email_valid) {
 		success = false;
