ec38261a3896ad4f3a5e6bbbbe29a422c4e11ad4
Spencer Gennari <sdgennari@gmail.com>
2016-04-09 05:54:12 -0400

    Fixed minor style bugs. Added dashboard link to header and footer

--- lytics/get_json_heatmap_data.php
+++ lytics/get_json_heatmap_data.php
@@ -1,4 +1,9 @@
 <?php
+	session_start();
+	if (!isset($_SESSION['first_name'])) {
+		header("Location: index.php");
+	}
+
 	$str = file_get_contents("raw_data/robinhood_heatmap_data.json");
 	$json = json_decode($str, true);
 	echo json_encode($json);
--- lytics/sign_up_confirm.php
+++ lytics/sign_up_confirm.php
@@ -1,5 +1,8 @@
 <?php
 	session_start();
+	if (!isset($_SESSION['first_name'])) {
+		header("Location: index.php");
+	}
 ?>
 
 <!DOCTYPE html>
